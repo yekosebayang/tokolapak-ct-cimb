323b1e28f75170f0fbacf78cc8ab3f09f1ae9763
tmangowal <tmangowal@gmail.com>
2020-04-25 13:34:04 +0700

    :truck: Added assets and navigation on home page

--- src/views/screens/Home/Home.jsx
+++ src/views/screens/Home/Home.jsx
@@ -5,9 +5,11 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        Home
-        <div>
-          <ProductCard />
+        <div className="d-flex justify-content-center flex-row align-items-center mt-4">
+          <h6 className="mx-4 font-weight-bold">PHONE</h6>
+          <h6 className="mx-4 font-weight-bold">LAPTOP</h6>
+          <h6 className="mx-4 font-weight-bold">TAB</h6>
+          <h6 className="mx-4 font-weight-bold">DESKTOP</h6>
         </div>
       </div>
     );
