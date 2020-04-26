82080d8b4a1119d9a7542faa641fd842cdc0525a
tmangowal <tmangowal@gmail.com>
2020-04-27 00:59:42 +0700

    :carousel_horse: Added carousel on home page

--- src/views/components/Button/Button.tsx
+++ src/views/components/Button/Button.tsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import "./Button.css";
 
 type ButtonTypes = {
-  type: "contained" | "outlined" | "textual";
+  type?: "contained" | "outlined" | "textual";
   children: any;
-  style?: object;
+  style?: CSSProperties;
   className?: string;
 };
 
 const ButtonUI = (props: ButtonTypes) => {
-  const { type, children, style, className } = props;
+  let { type, children, style, className } = props;
+
+  type = type || "contained";
 
   return (
     <div style={style} className={`custom-btn custom-btn-${type} ${className}`}>
