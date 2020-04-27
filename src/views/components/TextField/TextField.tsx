b3d57fa4d4c7075e9d991868afaa7eaced7bd92e
tmangowal <tmangowal@gmail.com>
2020-04-27 12:06:35 +0700

    :abcd: TextField update

--- src/views/components/TextField/TextField.tsx
+++ src/views/components/TextField/TextField.tsx
@@ -5,6 +5,7 @@ type TextFieldProps = {
   focused?: boolean;
   className: string;
   placeholder: string;
+  onChange: any;
 };
 
 class TextField extends React.Component<TextFieldProps> {
@@ -24,6 +25,7 @@ class TextField extends React.Component<TextFieldProps> {
   render() {
     return (
       <input
+        onChange={this.props.onChange}
         placeholder={this.props.placeholder}
         type="text"
         onFocus={this.onFocus}
