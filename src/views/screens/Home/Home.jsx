0b801ab3b737366325c38efec5da52309997d28c
tmangowal <tmangowal@gmail.com>
2020-04-27 10:21:17 +0700

    test pull

--- src/views/screens/Home/Home.jsx
+++ src/views/screens/Home/Home.jsx
@@ -154,7 +154,6 @@ class Home extends React.Component {
             <ProductCard className="m-2" />
             <ProductCard className="m-2" />
             <ProductCard className="m-2" />
-            <ProductCard className="m-2" />
           </div>
         </div>
         {/* ABOUT SECTION */}
