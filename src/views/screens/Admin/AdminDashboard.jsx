508caeefe1de3177c3eb76aa793779a83e1dbb23
tmangowal <tmangowal@gmail.com>
2020-04-30 17:56:09 +0700

    Edit product

--- src/views/screens/Admin/AdminDashboard.jsx
+++ src/views/screens/Admin/AdminDashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./AdminDashboard.css";
-import { Collapse, Button } from "reactstrap";
+import { Modal, ModalHeader, ModalBody } from "reactstrap";
 import Axios from "axios";
 
 import { API_URL } from "../../../constants/API";
@@ -29,6 +29,7 @@ class AdminDashboard extends React.Component {
       desc: "",
     },
     activeProducts: [],
+    modalOpen: false,
   };
 
   getProductList = () => {
@@ -122,7 +123,7 @@ class AdminDashboard extends React.Component {
   };
 
   inputHandler = (e, field, form) => {
-    const { value } = e.target;
+    let { value } = e.target;
     this.setState({
       [form]: {
         ...this.state[form],
@@ -156,6 +157,7 @@ class AdminDashboard extends React.Component {
       editForm: {
         ...this.state.productList[idx],
       },
+      modalOpen: true,
     });
   };
 
@@ -166,12 +168,17 @@ class AdminDashboard extends React.Component {
     )
       .then((res) => {
         swal("Success!", "Your item has been edited", "success");
+        this.setState({ modalOpen: false });
         this.getProductList();
       })
       .catch((err) => {
         swal("Error!", "Your item could not be edited", "error");
         console.log(err);
       });
+  };
+
+  toggleModal = () => {
+    this.setState({ modalOpen: !this.state.modalOpen });
   };
 
   componentDidMount() {
@@ -194,118 +201,144 @@ class AdminDashboard extends React.Component {
               </tr>
             </thead>
             <tbody>{this.renderProductList()}</tbody>
-            {/* 
-            <tfoot>
-              <tr>
-                <td colSpan={2}>
-                  <TextField
-                    value={this.state.createForm.productName}
-                    onChange={(e) =>
-                      this.inputHandler(e, "productName", "createForm")
-                    }
-                    placeholder="Name"
-                  />
-                </td>
-                <td>
-                  <TextField
-                    value={this.state.createForm.price}
-                    onChange={(e) => this.inputHandler(e, "price", "createForm")}
-                    placeholder="Price"
-                  />
-                </td>
-                <td colSpan={2}>
-                  <select
-                    value={this.state.createForm.category}
-                    onChange={(e) =>
-                      this.inputHandler(e, "category", "createForm")
-                    }
-                    className="form-control"
-                  >
-                    <option value="Phone">Phone</option>
-                    <option value="Laptop">Laptop</option>
-                    <option value="Tab">Tab</option>
-                    <option value="Desktop">Desktop</option>
-                  </select>
-                </td>
-                <td>
-                  <TextField
-                    value={this.state.createForm.image}
-                    onChange={(e) => this.inputHandler(e, "image", "createForm")}
-                    placeholder="Image"
-                  />
-                </td>
-                <td colSpan={2}>
-                  <TextField
-                    value={this.state.createForm.desc}
-                    onChange={(e) => this.inputHandler(e, "desc", "createForm")}
-                    placeholder="Description"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td colSpan={7}></td>
-                <td colSpan={1}>
-                  <ButtonUI onClick={this.createProductHandler} type="contained">
-                    Create
-                  </ButtonUI>
-                </td>
-              </tr>
-              <tr>
-                <td colSpan={2}>
-                  <TextField
-                    value={this.state.editForm.productName}
-                    onChange={(e) =>
-                      this.inputHandler(e, "productName", "editForm")
-                    }
-                    placeholder="Name"
-                  />
-                </td>
-                <td>
-                  <TextField
-                    value={this.state.editForm.price}
-                    onChange={(e) => this.inputHandler(e, "price", "editForm")}
-                    placeholder="Price"
-                  />
-                </td>
-                <td colSpan={2}>
-                  <select
-                    value={this.state.editForm.category}
-                    onChange={(e) => this.inputHandler(e, "category", "editForm")}
-                    className="form-control"
-                  >
-                    <option value="Phone">Phone</option>
-                    <option value="Laptop">Laptop</option>
-                    <option value="Tab">Tab</option>
-                    <option value="Desktop">Desktop</option>
-                  </select>
-                </td>
-                <td>
-                  <TextField
-                    value={this.state.editForm.image}
-                    onChange={(e) => this.inputHandler(e, "image", "editForm")}
-                    placeholder="Image"
-                  />
-                </td>
-                <td colSpan={2}>
-                  <TextField
-                    value={this.state.editForm.desc}
-                    onChange={(e) => this.inputHandler(e, "desc", "editForm")}
-                    placeholder="Description"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td colSpan={7}></td>
-                <td colSpan={1}>
-                  <ButtonUI onClick={this.editProductHandler} type="contained">
-                    Save
-                  </ButtonUI>
-                </td>
-              </tr>
-            </tfoot>
-          */}
           </table>
         </div>
+        <div className="dashboard-form-container p-4">
+          <caption className="mb-4 mt-2">
+            <h2>Add Product</h2>
+          </caption>
+          <div className="row">
+            <div className="col-8">
+              <TextField
+                value={this.state.createForm.productName}
+                placeholder="Product Name"
+                onChange={(e) =>
+                  this.inputHandler(e, "productName", "createForm")
+                }
+              />
+            </div>
+            <div className="col-4">
+              <TextField
+                value={this.state.createForm.price}
+                placeholder="Price"
+                onChange={(e) => this.inputHandler(e, "price", "createForm")}
+              />
+            </div>
+            <div className="col-12 mt-3">
+              <textarea
+                value={this.state.createForm.desc}
+                onChange={(e) => this.inputHandler(e, "desc", "createForm")}
+                style={{ resize: "none" }}
+                placeholder="Description"
+                className="custom-text-input"
+              ></textarea>
+            </div>
+            <div className="col-6 mt-3">
+              <TextField
+                value={this.state.createForm.image}
+                placeholder="Image Source"
+                onChange={(e) => this.inputHandler(e, "image", "createForm")}
+              />
+            </div>
+            <div className="col-6 mt-3">
+              <select
+                value={this.state.createForm.category}
+                className="custom-text-input h-100 pl-3"
+                onChange={(e) => this.inputHandler(e, "category", "createForm")}
+              >
+                <option value="Phone">Phone</option>
+                <option value="Tab">Tab</option>
+                <option value="Laptop">Laptop</option>
+                <option value="Desktop">Desktop</option>
+              </select>
+            </div>
+            <div className="col-3 mt-3">
+              <ButtonUI onClick={this.createProductHandler} type="contained">
+                Create Product
+              </ButtonUI>
+            </div>
+          </div>
+        </div>
+        <Modal
+          toggle={this.toggleModal}
+          isOpen={this.state.modalOpen}
+          className="edit-modal"
+        >
+          <ModalHeader toggle={this.toggleModal}>
+            <caption>
+              <h3>Edit Product</h3>
+            </caption>
+          </ModalHeader>
+          <ModalBody>
+            <div className="row">
+              <div className="col-8">
+                <TextField
+                  value={this.state.editForm.productName}
+                  placeholder="Product Name"
+                  onChange={(e) =>
+                    this.inputHandler(e, "productName", "editForm")
+                  }
+                />
+              </div>
+              <div className="col-4">
+                <TextField
+                  value={this.state.editForm.price}
+                  placeholder="Price"
+                  onChange={(e) => this.inputHandler(e, "price", "editForm")}
+                />
+              </div>
+              <div className="col-12 mt-3">
+                <textarea
+                  value={this.state.editForm.desc}
+                  onChange={(e) => this.inputHandler(e, "desc", "editForm")}
+                  style={{ resize: "none" }}
+                  placeholder="Description"
+                  className="custom-text-input"
+                ></textarea>
+              </div>
+              <div className="col-6 mt-3">
+                <TextField
+                  value={this.state.editForm.image}
+                  placeholder="Image Source"
+                  onChange={(e) => this.inputHandler(e, "image", "editForm")}
+                />
+              </div>
+              <div className="col-6 mt-3">
+                <select
+                  value={this.state.editForm.category}
+                  className="custom-text-input h-100 pl-3"
+                  onChange={(e) => this.inputHandler(e, "category", "editForm")}
+                >
+                  <option value="Phone">Phone</option>
+                  <option value="Tab">Tab</option>
+                  <option value="Laptop">Laptop</option>
+                  <option value="Desktop">Desktop</option>
+                </select>
+              </div>
+              <div className="col-12 text-center my-3">
+                <img src={this.state.editForm.image} alt="" />
+              </div>
+              <div className="col-5 mt-3 offset-1">
+                <ButtonUI
+                  className="w-100"
+                  onClick={this.toggleModal}
+                  type="outlined"
+                >
+                  Cancel
+                </ButtonUI>
+              </div>
+              <div className="col-5 mt-3">
+                <ButtonUI
+                  className="w-100"
+                  onClick={this.editProductHandler}
+                  type="contained"
+                >
+                  Save
+                </ButtonUI>
+              </div>
+            </div>
+          </ModalBody>
+        </Modal>
       </div>
     );
   }
